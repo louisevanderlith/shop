0dd4c59014f465440512fb8156a4a0ada57d9f15
louisevanderlith <louisevanderlith@gmail.com>
2020-10-28 10:17:58 +0200

    Updated Auth

--- handles/handler.go
+++ handles/handler.go
@@ -1,13 +1,54 @@
 package handles
 
 import (
+	"github.com/coreos/go-oidc"
 	"github.com/gorilla/mux"
 	"github.com/louisevanderlith/droxolite/drx"
-	"github.com/louisevanderlith/kong/middle"
+	"github.com/louisevanderlith/droxolite/open"
+	"github.com/louisevanderlith/theme/api"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/clientcredentials"
 	"net/http"
 )
 
-func SetupRoutes(clnt, scrt, securityUrl, managerUrl, authorityUrl string) http.Handler {
+var (
+	CredConfig *clientcredentials.Config
+	Endpoints map[string]string
+	//StockURL   string
+)
+
+func SetupRoutes(host, clientId, clientSecret string, endpoints map[string]string) http.Handler {
+	ctx := context.Background()
+	provider, err := oidc.NewProvider(ctx, endpoints["issuer"])
+
+	if err != nil {
+		panic(err)
+	}
+
+	Endpoints = endpoints
+
+	authConfig := &oauth2.Config{
+		ClientID:     clientId,
+		ClientSecret: clientSecret,
+		Endpoint:     provider.Endpoint(),
+		RedirectURL:  host + "/callback",
+		Scopes:       []string{oidc.ScopeOpenID},
+	}
+
+	CredConfig = &clientcredentials.Config{
+		ClientID:     clientId,
+		ClientSecret: clientSecret,
+		TokenURL:     provider.Endpoint().TokenURL,
+		Scopes:       []string{oidc.ScopeOpenID, "theme"},
+	}
+
+	err = api.UpdateTemplate(CredConfig.Client(ctx), endpoints["theme"])
+
+	if err != nil {
+		panic(err)
+	}
+
 	tmpl, err := drx.LoadTemplate("./views")
 
 	if err != nil {
@@ -20,13 +61,19 @@ func SetupRoutes(clnt, scrt, securityUrl, managerUrl, authorityUrl string) http.
 	fs := http.FileServer(distPath)
 	r.PathPrefix("/dist/").Handler(http.StripPrefix("/dist/", fs))
 
-	clntIns := middle.NewClientInspector(clnt, scrt, http.DefaultClient, securityUrl, managerUrl, authorityUrl)
-	r.HandleFunc("/callback", clntIns.Callback).Queries("state", "{state}", "token", "{token}").Methods(http.MethodGet)
+	lock := open.NewUILock(authConfig)
+	r.HandleFunc("/login", lock.Login).Methods(http.MethodGet)
+	r.HandleFunc("/callback", lock.Callback).Methods(http.MethodGet)
 
-	r.HandleFunc("/", clntIns.Middleware(Index(tmpl), map[string]bool{"stock.clothing.search": true})).Methods(http.MethodGet)
-	r.HandleFunc("/{pagesize:[A-Z][0-9]+}", clntIns.Middleware(SearchAds(tmpl), map[string]bool{"stock.clothing.search": true})).Methods(http.MethodGet)
-	r.HandleFunc("/{pagesize:[A-Z][0-9]+}/{hash:[a-zA-Z0-9]+={0,2}}", clntIns.Middleware(SearchAds(tmpl), map[string]bool{"stock.clothing.search": true})).Methods(http.MethodGet)
-	r.HandleFunc("/{key:[0-9]+\\x60[0-9]+}", clntIns.Middleware(ViewAd(tmpl), map[string]bool{"stock.clothing.view": true})).Methods(http.MethodGet)
+	oidcConfig := &oidc.Config{
+		ClientID: clientId,
+	}
+	v := provider.Verifier(oidcConfig)
+
+	r.HandleFunc("/", open.LoginMiddleware(v, Index(tmpl))).Methods(http.MethodGet)
+	r.HandleFunc("/{pagesize:[A-Z][0-9]+}", open.LoginMiddleware(v, SearchAds(tmpl))).Methods(http.MethodGet)
+	r.HandleFunc("/{pagesize:[A-Z][0-9]+}/{hash:[a-zA-Z0-9]+={0,2}}", open.LoginMiddleware(v, SearchAds(tmpl))).Methods(http.MethodGet)
+	r.HandleFunc("/{key:[0-9]+\\x60[0-9]+}", open.LoginMiddleware(v, ViewAd(tmpl))).Methods(http.MethodGet)
 
 	return r
 }
